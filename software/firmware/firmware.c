a967f33d0a6bb2cd8bf968bcf111264d519b83ed
Pedro Antunes <pedronmantunes@gmail.com>
2022-05-06 02:40:05 +0100

    pass CLINT_BASE to clint software

--- software/firmware/firmware.c
+++ software/firmware/firmware.c
@@ -12,10 +12,12 @@ int main()
 
   //init uart
   uart_init(UART_BASE,FREQ/BAUD);
+  clint_init(CLINT_BASE);
+  
   uart_puts("\n\n\nHello world!\n\n\n");
   printf("Value of Pi = %f\n\n", 3.1415);
 
   elapsed = clint_get_timer();
-  printf("\nCLINT timer value: %d\n", (unsigned int) elapsed);
+  printf("\nCLINT timer value: %x\n", elapsed);
   uart_finish();
 }
