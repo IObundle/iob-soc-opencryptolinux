68337bce199d53813e57b5b983af5b2baf8522f9
Pedro Antunes <pedronmantunes@gmail.com>
2022-11-17 23:31:32 +0000

    Downgrade UART16550 submodule; Console should use default BAUD rate;

--- hardware/fpga/fpga.mk
+++ hardware/fpga/fpga.mk
@@ -18,7 +18,7 @@ INIT_MEM=0
 endif
 
 #console command
-CONSOLE_CMD=$(PYTHON_DIR)/console -s /dev/usb-uart -b $(BOARD_BAUD)
+CONSOLE_CMD=$(PYTHON_DIR)/console -s /dev/usb-uart
 ifeq ($(INIT_MEM),0)
 CONSOLE_CMD+=-f
 endif
