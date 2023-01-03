563fdd740a743b3c360454f71c4ac4ade6a5a14d
Pedro Antunes <pedronmantunes@gmail.com>
2023-01-03 16:59:51 +0000

    Nix-Shell: Update shell to test github actions with latest verilator verision

--- shell.nix
+++ shell.nix
@@ -1,6 +1,6 @@
-with (import <nixpkgs> {});
-mkShell {
-  nativeBuildInputs = [ 
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [ 
     verilog 
     verilator 
     gtkwave
