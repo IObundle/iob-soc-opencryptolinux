d25bfc724756b2d74cb4d0a6ad3a27831976a688
Artur Nóbrega <arturnobrega1999@gmail.com>
2024-05-23 05:50:27 +0100

    fix(eth): Fix multiple transfers from soc2eth.

--- software/src/iob_soc_opencryptolinux_boot.c
+++ software/src/iob_soc_opencryptolinux_boot.c
@@ -105,6 +105,8 @@ int main() {
   char eth_debug_data[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64};
   eth_send_frame(eth_debug_data, 64);
   uart16550_puts("D3\n"); //DEBUG
+  eth_send_frame(eth_debug_data, 64);
+  uart16550_puts("D4\n"); //DEBUG
 
   file_size = uart16550_recvfile("../iob_soc_opencryptolinux_mem.config", prog_start_addr);
   // compute_mem_load_txt
