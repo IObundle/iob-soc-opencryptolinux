8226c4da5756a60fc5947d49b86fe1b489d72326
Artur Nóbrega <arturnobrega1999@gmail.com>
2024-06-24 16:49:18 +0100

    fix(dma_demo): Fix minicom file transfers. Drivers do not initialize correctly. Probably due to missing mdev or device tree.

--- software/tests/dma/runTest.sh
+++ software/tests/dma/runTest.sh
@@ -1,9 +1,9 @@
 #!/bin/sh 
 
 # Load Linux Kernel Modules
-insmod dma/drivers/iob_dma.ko
-insmod dma/drivers/iob_axistream_in.ko
-insmod dma/drivers/iob_axistream_out.ko
+insmod iob_dma.ko
+insmod iob_axistream_in.ko
+insmod iob_axistream_out.ko
 
 # Run DMA demo
-./dma/dma_demo
+./dma_demo
